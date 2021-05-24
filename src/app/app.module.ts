23dcdf869166cfe1b1fb7b06d65299b40620d83b
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2021-05-25 00:31:36 +0200

    custom pipe & directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { EuroCurrencyPipe } from './euro-currency.pipe';
+import { HighlightDirective } from './highlight.directive';
+import { TooltipDirective } from './tooltip.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ProductListComponent
+    ProductListComponent,
+    EuroCurrencyPipe,
+    HighlightDirective,
+    TooltipDirective
   ],
   imports: [
     BrowserModule,
