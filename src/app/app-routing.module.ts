c61a5678156b8ad8efd6e8fc7f630c42a15d5351
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2021-05-26 21:29:58 +0200

    forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import {RouterModule, Routes} from '@angular/router';
 import {ProductsPageComponent} from "./products-page/products-page.component";
 import {ShoppingCartPageComponent} from "./shopping-cart-page/shopping-cart-page.component";
 import {ProductDetailComponent} from "./product-detail/product-detail.component";
+import {ProductAddComponent} from "./product-add/product-add.component";
 
 const routes: Routes = [
   {path: 'products', component: ProductsPageComponent},
   {path: 'product/:id', component: ProductDetailComponent},
+  {path: 'products/add', component: ProductAddComponent},
   {path: 'shopping-cart', component: ShoppingCartPageComponent},
   {path: '', redirectTo: '/products', pathMatch: 'full'},
 ];
